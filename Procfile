ff726797398418504a7b82ec526a97daea0cb23f
Philippe <philippe.desousaviolante@gmail.com>
2020-08-11 22:37:07 +0200

    Trying to fix the cors issues

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi --log-file -
+web: gunicorn --worker-class eventlet -w 1 server:app
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from flask import Flask, request
-from flask_socketio import SocketIO, send, emit
-from flask_cors import CORS, cross_origin
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import json
+
 
 from src.input_handler import handle_input, start_conversation
 from src.users import add_user, find_user
@@ -10,40 +11,18 @@ from src.users import add_user, find_user
 import requests
 import os
 
-application = Flask(__name__, static_url_path='/', static_folder='static/')
-application.config['SECRET_KEY'] = 'secret!'
-application.config['DEBUG'] = True
-socketio = SocketIO()
-
 PORT = 8080 if 'PORT' not in os.environ else os.environ['PORT']
 
-socketio.init_app(application)
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+	return app.send_static_file('index.html')
 
 @socketio.on('message')
-def handle_message(message):
-	user = find_user(request.sid)
-	response = handle_input(user, message)
-	emit('message', response)
+def handle_source(json_data):
+  socketio.emit('message', 'mdrrr')
 
-@socketio.on('connect')
-def handle_connect():
-	add_user(request.sid)
-	user = find_user(request.sid)
-	response = start_conversation(user)
-	emit('message', response)
-
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
-
-@application.route('/ui')
-def send_html():
-	return application.send_static_file('index.html')
-
-@application.route('/')
-def get_home():
-	return 'Chat service online on port ' + str(PORT)
-
-if __name__ == '__main__':
-	print('Starting server')
-	socketio.run(application, host='0.0.0.0', port=PORT)
+if __name__ == "__main__":
+  socketio.run(app)
