86e276cd7a8a2eb901b6f45b6638d49e86bd5a95
Philippe <philippe.desousaviolante@gmail.com>
2020-09-15 11:40:43 +0200

    Logging messages

--- server.py
+++ server.py
@@ -37,6 +37,7 @@ def download_report(report_id):
 
 @socketio.on('message')
 def handle_message(message):
+    print(message)
     user = find_user(request.sid)
     response = handle_input(user, message)
     emit('message', response)
